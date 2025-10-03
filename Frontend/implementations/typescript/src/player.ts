8bd648d5db50569d93532e27691fb8d24968b774
Themistoklis Arvanitis <themistoklisarv2001@gmail.com>
2025-10-03 11:41:27 +0200

    Fixed Dependencies

--- Frontend/implementations/typescript/src/player.ts
+++ Frontend/implementations/typescript/src/player.ts
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-export * from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.5';
-export * from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.5';
+// export * from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.5';
+// export * from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.5';
 import { Config, PixelStreaming, Flags } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.4';
 import { Application, PixelStreamingApplicationStyle } from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.4';
 const PixelStreamingApplicationStyles =
@@ -20,7 +20,7 @@ document.body.onload = function() {
 	// Create a config object
 	const config = new Config({ useUrlParams: true });
 	config.setFlagEnabled(Flags.HoveringMouseMode, true);
-	config.setFlagEnabled(Flags.SimulateTouch, true);
+	config.setFlagEnabled(Flags.TouchInput, true);
 	// Create a Native DOM delegate instance that implements the Delegate interface class
 	const stream = new PixelStreaming(config);
 
